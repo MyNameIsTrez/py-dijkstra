9d123bcc089bce1849fd2342563d00adb5bef34d
MyNameIsTrez <welfje@gmail.com>
2022-05-06 02:31:40 +0200

    Refactor by tracking unvisited, instead of visited. Add showcase_acyclic().

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # py-dijkstra
 
 A Python class for the Dijkstra pathfinding algorithm.
+
+It returns the shortest path.
+
+It handles both acyclic and cyclic graphs.
--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import heapq
 
 
 def main():
-	graph = {
+	showcase_cyclic()
+	showcase_acyclic()
+
+
+def showcase_cyclic():
+	cyclic_graph = {
 		"a": { "w": 14, "x": 7, "y": 9 },
 		"b": { "w": 9, "z": 6 },
 		"w": { "a": 14, "b": 9, "y": 2 },
@@ -11,60 +16,80 @@ def main():
 		"z": { "b": 6, "x": 15, "y": 11 }
 	}
 
-	print(dijkstra(graph, "a", "a")) # (0, ["a"])
-	print(dijkstra(graph, "a", "b")) # (20, ["a", "y", "w", "b"])
+	dijkstra = Dijkstra(cyclic_graph)
+
+	print(dijkstra.run("a", "a")) # ['a']
+	print(dijkstra.run("a", "b")) # ['a', 'y', 'w', 'b']
 
 
-def dijkstra(graph, start, end):
-	distances = { key: float("inf") for key in graph.keys() }
-	distances[start] = 0
+def showcase_acyclic():
+	acyclic_graph = {
+		"a": { "b": 1, "c": 2 },
+		"b": { "d": 3 },
+		"c": { "d": 4 },
+		"d": { "e": 5 },
+		"e": {}
+	}
 
-	visited = set()
-	predecessors = {}
+	dijkstra = Dijkstra(acyclic_graph)
 
-	node = start
-
-	while node != end:
-		visited.add(node)
-
-		for neighbor in graph[node].keys():
-			if neighbor not in visited:
-				tentative_distance = distances[node] + graph[node][neighbor]
-				recorded_distance = distances[neighbor]
-
-				if tentative_distance < recorded_distance:
-					distances[neighbor] = tentative_distance
-					predecessors[neighbor] = node
-
-		node = get_closest_node(graph, visited, distances)
-
-	return distances[node], get_path(end, predecessors)
+	print(dijkstra.run("a", "a")) # ['a']
+	print(dijkstra.run("a", "e")) # ['a', 'b', 'd', 'e']
 
 
-def get_closest_node(graph, visited, distances):
-	unvisited = []
-
-	for k in graph:
-		if k not in visited:
-			unvisited.append((distances[k], k))
-
-	heapq.heapify(unvisited)
-
-	_, closest_node = heapq.heappop(unvisited)
-
-	return closest_node
+class Dijkstra:
+	def __init__(self, graph):
+		self.graph = graph
 
 
-def get_path(end, predecessors):
-	path = []
+	def run(self, start, end):
+		distances = { key: float("inf") for key in self.graph.keys() }
+		distances[start] = 0
 
-	while True:
-		path.append(end)
+		unvisited = { key for key in self.graph.keys() }
 
-		if end not in predecessors:
-			return list(reversed(path))
+		parents = {}
 
-		end = predecessors[end]
+		node = start
+
+		while node != end:
+			unvisited.remove(node)
+
+			for neighbor in self.graph[node].keys():
+				if neighbor in unvisited:
+					tentative_distance = distances[node] + self.graph[node][neighbor]
+					recorded_distance = distances[neighbor]
+
+					if tentative_distance < recorded_distance:
+						distances[neighbor] = tentative_distance
+						parents[neighbor] = node
+
+			node = self.get_closest_node(unvisited, distances)
+
+		return self.get_path(end, parents)
+
+
+	def get_closest_node(self, unvisited, distances):
+		unvisited_min_heap = [(distances[node], node) for node in unvisited]
+
+		# Great explanation of heapify: https://stackoverflow.com/a/61446534/13279557
+		heapq.heapify(unvisited_min_heap)
+
+		_, closest_node = heapq.heappop(unvisited_min_heap)
+
+		return closest_node
+
+
+	def get_path(self, end, parents):
+		path = []
+
+		while True:
+			path.append(end)
+
+			if end not in parents:
+				return list(reversed(path))
+
+			end = parents[end]
 
 
 if __name__ == "__main__":
